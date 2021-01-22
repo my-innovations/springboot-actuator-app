2a306816e3aa29c34fcf3f56d109a32e86cf8660
DELL <punyasmrutin@gmail.com>
2021-01-22 23:28:28 +0530

    Initial commit

---
+++ src/main/java/com/mightyjava/controller/NoMappingFoundController.java
@@ -0,0 +1,21 @@
+package com.mightyjava.controller;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoMappingFoundController implements ErrorController {
+
+	private static final String PATH = "/error";
+
+	@Override
+	public String getErrorPath() {
+		return PATH;
+	}
+
+	@RequestMapping(PATH)
+	public String error() {
+		return "No Resource Mapping available.";
+	}
+}
